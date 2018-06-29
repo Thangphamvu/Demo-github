fe7c1884555799894e9aab124d2994f46bc88196
unknown <Administrator@PC22046.MTSVIETNAM>
2018-06-29 14:47:55 +0700

    tam tam the nay thoi da

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="h1">todos</h1>
+        <div className="main">
+          <div className="todo">
+            <input className="new-todo" type="text" placeholder="What needs to be done?" />
+          </div>
+          <div>
+            <input className="toggle-all" type="checkbox" />
+            <ul>
+              <li>
+                <input className="toggle" type="checkbox" />
+                <label>abc</label>
+                <button className="btn btn-danger delete">
+                  <i className="fa fa-times-circle"></i>
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className="footer">
+            <div className="left">
+              count items left
+            </div>
+            <div className="center">
+              some button
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
